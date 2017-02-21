75913e0de7e765ae20ca79dc93afb97584270f5c
CPI-Trainee <CPI-Trainee@TRNG-PC14>
2017-02-21 18:07:48 +0800

    peripherals

--- src/com/ims/controllers/peripherals/PeripheralsController.java
+++ src/com/ims/controllers/peripherals/PeripheralsController.java
@@ -1,7 +1,9 @@
 package com.ims.controllers.peripherals;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -13,6 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.ims.model.peripherals.Peripherals;
 import com.ims.service.peripherals.PeripheralsService;
 
@@ -38,7 +42,6 @@ public class PeripheralsController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		ApplicationContext context = new ClassPathXmlApplicationContext("/com/ims/resource/beans.xml");
-
 		PeripheralsService service = (PeripheralsService) context.getBean("servicePeripheralsBean");
 
 		try {
@@ -59,8 +62,32 @@ public class PeripheralsController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		ApplicationContext context = new ClassPathXmlApplicationContext("/com/ims/resource/beans.xml");
+		PeripheralsService service = (PeripheralsService) context.getBean("servicePeripheralsBean");
+
+		String action = request.getParameter("action");
+		if(action.equals("save")){
+			Gson gson = new Gson();
+			String json = request.getParameter("peripherals");
+			Type collectionType = new TypeToken<ArrayList<Peripherals>>() {}.getType();
+			List<Peripherals> peripherals = new Gson().fromJson(json, collectionType);
+			
+			for(Peripherals p : peripherals){
+				System.out.println(p.getUnitNo());
+				System.out.println(p.getPeripheralNo());
+				System.out.println(p.getPeripheralType());
+				System.out.println(p.getTagNumber());
+				System.out.println(p.getAcquiredDate());
+				System.out.println(p.getDescription());
+				System.out.println(p.getSerialNo());
+				System.out.println(p.getBrand());
+				System.out.println(p.getModel());
+				System.out.println(p.getColor());
+				System.out.println(p.getRemarks());
+				System.out.println(p.getUserId());
+				System.out.println(p.getLastUpdate());
+			}
+		}
 	}
 
 }
