1ab4b20c657a696f79409abe8955049fbc2ce54f
PCI-Trainee <PCI-Trainee@TRNG-PC10>
2017-02-23 19:09:56 +0800

    home and login

--- src/com/ims/service/impl/homeandlogin/LoginServiceImpl.java
+++ src/com/ims/service/impl/homeandlogin/LoginServiceImpl.java
@@ -1,7 +1,25 @@
 package com.ims.service.impl.homeandlogin;
 
+import java.sql.SQLException;
+
+import com.ims.dao.impl.homeandlogin.LoginDaoImpl;
 import com.ims.service.homeandlogin.LoginService;
 
 public class LoginServiceImpl implements LoginService{
+
+	private LoginDaoImpl dao;
+	
+	@Override
+	public String getLoginId(String id) throws SQLException {
+		return this.getDao().getLoginId(id);
+	}
+
+	public LoginDaoImpl getDao() {
+		return dao;
+	}
+
+	public void setDao(LoginDaoImpl dao) {
+		this.dao = dao;
+	}
 
 }
