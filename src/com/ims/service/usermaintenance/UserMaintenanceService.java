68f4d464be569bde12dbde82f9b6e0865799d1ea
jbmapili <jbzjhan@gmail.com>
2017-02-24 09:22:55 +0800

    user login service and web.xml

--- src/com/ims/service/usermaintenance/UserMaintenanceService.java
+++ src/com/ims/service/usermaintenance/UserMaintenanceService.java
@@ -14,4 +14,6 @@ public interface UserMaintenanceService {
 	String getUserId(String id) throws SQLException;
 	List<Users> getUser(String uId) throws SQLException;
 	void updateUser(HttpServletRequest request) throws SQLException; 
+	String getPassword(String username) throws SQLException;
+	String getUserAccess(String username) throws SQLException;
 }
