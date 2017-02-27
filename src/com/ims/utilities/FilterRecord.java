62814aca6ed3578e22c670486b54cf879812d693
CPI-Trainee <CPI-Trainee@TRNG-PC15>
2017-02-27 11:58:04 +0800

    filter

--- src/com/ims/utilities/FilterRecord.java
+++ src/com/ims/utilities/FilterRecord.java
@@ -9,38 +9,33 @@ import com.ims.model.usermaintenance.Users;
 
 public class FilterRecord {
 	public static List<ComputerUnits> getFilterCompUnits(List<ComputerUnits> computerUnits, String words) {
-		//TODO-Jenny : filter words only on columns being shown on html table
-		
+
 		List<ComputerUnits> filterList = new LinkedList<>();
 		for (int x = 0; x < computerUnits.size(); x++) {
+			String type = computerUnits.get(x).getType().equals("DT") ? "Desktop" : "Laptop";
 			if (computerUnits.get(x).getUnitName().toUpperCase().contains(words)
-					|| computerUnits.get(x).getModel().toUpperCase().contains(words)
 					|| computerUnits.get(x).getAcquiredDate().toString().toUpperCase().contains(words)
 					|| computerUnits.get(x).getBrand().toUpperCase().contains(words)
-					|| computerUnits.get(x).getColor().toUpperCase().contains(words)
-					|| computerUnits.get(x).getDescription().toUpperCase().contains(words)
 					|| computerUnits.get(x).getIpAddress().toUpperCase().contains(words)
-					|| computerUnits.get(x).getLastUpdate().toString().toUpperCase().contains(words)
 					|| computerUnits.get(x).getModel().toUpperCase().contains(words)
-					|| computerUnits.get(x).getUserId().toUpperCase().contains(words)
 					|| computerUnits.get(x).getUnitNo().toString().toUpperCase().contains(words)
 					|| computerUnits.get(x).getTagNumber().toUpperCase().contains(words)
 					|| computerUnits.get(x).getSerialNo().toUpperCase().contains(words)
-					|| computerUnits.get(x).getRemarks().toUpperCase().contains(words)) {
-
+					|| type.toUpperCase().contains(words)) {
 				filterList.add(computerUnits.get(x));
 			}
 		}
 		return filterList;
 	}
 
-	public static List<UnitAssignmentHist> getFilterUnitAssignmentHist(List<UnitAssignmentHist> unitAssignmentHist, String words) {
+	public static List<UnitAssignmentHist> getFilterUnitAssignmentHist(List<UnitAssignmentHist> unitAssignmentHist,
+			String words) {
 		List<UnitAssignmentHist> filterList = new LinkedList<>();
 		for (int x = 0; x < unitAssignmentHist.size(); x++) {
 			if (unitAssignmentHist.get(x).getHistNo().toString().toUpperCase().contains(words)
 					|| unitAssignmentHist.get(x).getUnitNo().toString().toUpperCase().contains(words)
 					|| unitAssignmentHist.get(x).getUnitName().toString().toUpperCase().contains(words)
-					|| unitAssignmentHist.get(x).getAssigneeNo().toUpperCase().contains(words)){
+					|| unitAssignmentHist.get(x).getAssigneeNo().toUpperCase().contains(words)) {
 				// TODO-Phyllis do the rest of the getters
 				filterList.add(unitAssignmentHist.get(x));
 			}
@@ -55,7 +50,7 @@ public class FilterRecord {
 					|| users.get(x).getMiddleInitial().toUpperCase().contains(words)
 					|| users.get(x).getLastName().toString().toUpperCase().contains(words)
 					|| users.get(x).getEmail().toUpperCase().contains(words)) {
-				//TODO-Sharlene do the rest of the getters
+				// TODO-Sharlene do the rest of the getters
 				filterList.add(users.get(x));
 			}
 		}
